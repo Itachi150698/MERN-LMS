93f03532ff65184092217e963855536a904f2846
youth1506 <shivampabrekar123@gmail.com>
2025-04-22 17:00:21 +0530

    Initial commit - MERN Udemy Clone

---
+++ client/src/components/common-form/form-controls.jsx
@@ -0,0 +1,64 @@
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+
+function FormControls({ formControls = [], formData, setFormData }) {
+  function renderComponentByType(getControlItem) {
+    let element = null;
+
+    switch (getControlItem.componentType) {
+      case "input":
+        element = <Input 
+        id={getControlItem.name}
+        name={getControlItem.name}
+        placeholder={getControlItem.placeholder}
+        type={getControlItem.type}
+        />;
+        break;
+      case "select":
+        element = <Select>
+            <SelectTrigger className="w-full">
+                <SelectValue placeholder={getControlItem.label}/>
+            </SelectTrigger>
+            <SelectContent>
+                {
+                    getControlItem.options && getControlItem.options.length > 0 ?
+                    getControlItem.options.map(optionItem=> <SelectItem key={optionItem.id} value={optionItem.id}>{optionItem.label}</SelectItem>) : null
+                }
+            </SelectContent>
+        </Select>;
+        break;
+      case "textarea":
+        element = <Textarea
+        id={getControlItem.name}
+        name={getControlItem.name}
+        placeholder={getControlItem.placeholder}
+        />;
+        break;
+      default:
+        element = <Input
+        id={getControlItem.name}
+        name={getControlItem.name}
+        placeholder={getControlItem.placeholder}
+        type={getControlItem.type}
+        />;
+        break;
+    }
+    return element;
+  }
+
+  return (
+    <div className="flex flex-col gap-3">
+      {formControls.map((controlItems) => (
+        <div key={controlItems.name}>
+          <Label htmlFor={controlItems.name}>
+            {controlItems.label}
+            {renderComponentByType(controlItems)}
+          </Label>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default FormControls;
---
+++ client/src/config/index.jsx
@@ -0,0 +1,39 @@
+export const signUpFormControls = [
+    {
+        name: 'username',
+        label: 'User Name',
+        placeholder: 'Enter your user name',
+        type: 'text',
+        componentType: 'input'
+    },
+    {
+        name: 'email',
+        label: 'User email',
+        placeholder: 'Enter your user email',
+        type: 'email',
+        componentType: 'input'
+    },
+    {
+        name: 'password',
+        label: 'Password',
+        placeholder: 'Enter your password',
+        type: 'password',
+        componentType: 'input'
+    }
+];
+export const signInFormControls = [
+    {
+        name: 'email',
+        label: 'User email',
+        placeholder: 'Enter your user email',
+        type: 'email',
+        componentType: 'input'
+    },
+    {
+        name: 'password',
+        label: 'Password',
+        placeholder: 'Enter your password',
+        type: 'password',
+        componentType: 'input'
+    }
+]
