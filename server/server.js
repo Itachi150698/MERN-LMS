ef33191457b40ebcb6818af1f1b7eb306c4fcaca
youth1506 <shivampabrekar123@gmail.com>
2025-06-12 00:45:02 +0530

    instructor routes

--- server/server.js
+++ server/server.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth-routes/index')
-const mediaRoutes = require('./routes/instructor-routes/media-routes')
+const authRoutes = require('./routes/auth-routes/index');
+const mediaRoutes = require('./routes/instructor-routes/media-routes');
+const instructorCourseRoutes = require('./routes/instructor-routes/course-routes');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -23,6 +24,7 @@ mongoose.connect(MONGO_URI).then(() => console.log('mongodb is connected')).catc
 //routes configuration
 app.use("/auth", authRoutes);
 app.use("/media", mediaRoutes);
+app.use("/instructor/course", instructorCourseRoutes);
 
 app.use((err, req, res, next) => {
     console.log(err.stack);
